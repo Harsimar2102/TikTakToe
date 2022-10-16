506d5d19c5aaca6a68ab15e2a79b432731f2baab
Harsimar2102 <102385932+Harsimar2102@users.noreply.github.com>
2022-10-16 00:30:52 -0400

    Add files via upload

---
+++ HKBhatiaAssignment1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace HKBhatiaAssignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B4 = new System.Windows.Forms.Button();
+            this.B6 = new System.Windows.Forms.Button();
+            this.B5 = new System.Windows.Forms.Button();
+            this.B7 = new System.Windows.Forms.Button();
+            this.B8 = new System.Windows.Forms.Button();
+            this.B9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // B1
+            // 
+            this.B1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B1.BackgroundImage")));
+            this.B1.Location = new System.Drawing.Point(12, 12);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(150, 150);
+            this.B1.TabIndex = 0;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.play);
+            // 
+            // B2
+            // 
+            this.B2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B2.BackgroundImage")));
+            this.B2.Location = new System.Drawing.Point(199, 12);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(150, 150);
+            this.B2.TabIndex = 0;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.play);
+            // 
+            // B3
+            // 
+            this.B3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B3.BackgroundImage")));
+            this.B3.Location = new System.Drawing.Point(386, 12);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(150, 150);
+            this.B3.TabIndex = 0;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.play);
+            // 
+            // B4
+            // 
+            this.B4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B4.BackgroundImage")));
+            this.B4.Location = new System.Drawing.Point(12, 210);
+            this.B4.Name = "B4";
+            this.B4.Size = new System.Drawing.Size(150, 150);
+            this.B4.TabIndex = 0;
+            this.B4.UseVisualStyleBackColor = true;
+            this.B4.Click += new System.EventHandler(this.play);
+            // 
+            // B6
+            // 
+            this.B6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B6.BackgroundImage")));
+            this.B6.Location = new System.Drawing.Point(386, 210);
+            this.B6.Name = "B6";
+            this.B6.Size = new System.Drawing.Size(150, 150);
+            this.B6.TabIndex = 0;
+            this.B6.UseVisualStyleBackColor = true;
+            this.B6.Click += new System.EventHandler(this.play);
+            // 
+            // B5
+            // 
+            this.B5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B5.BackgroundImage")));
+            this.B5.Location = new System.Drawing.Point(199, 210);
+            this.B5.Name = "B5";
+            this.B5.Size = new System.Drawing.Size(150, 150);
+            this.B5.TabIndex = 0;
+            this.B5.UseVisualStyleBackColor = true;
+            this.B5.Click += new System.EventHandler(this.play);
+            // 
+            // B7
+            // 
+            this.B7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B7.BackgroundImage")));
+            this.B7.Location = new System.Drawing.Point(12, 409);
+            this.B7.Name = "B7";
+            this.B7.Size = new System.Drawing.Size(150, 150);
+            this.B7.TabIndex = 0;
+            this.B7.UseVisualStyleBackColor = true;
+            this.B7.Click += new System.EventHandler(this.play);
+            // 
+            // B8
+            // 
+            this.B8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B8.BackgroundImage")));
+            this.B8.Location = new System.Drawing.Point(199, 409);
+            this.B8.Name = "B8";
+            this.B8.Size = new System.Drawing.Size(150, 150);
+            this.B8.TabIndex = 0;
+            this.B8.UseVisualStyleBackColor = true;
+            this.B8.Click += new System.EventHandler(this.play);
+            // 
+            // B9
+            // 
+            this.B9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("B9.BackgroundImage")));
+            this.B9.Location = new System.Drawing.Point(386, 409);
+            this.B9.Name = "B9";
+            this.B9.Size = new System.Drawing.Size(150, 150);
+            this.B9.TabIndex = 0;
+            this.B9.UseVisualStyleBackColor = true;
+            this.B9.Click += new System.EventHandler(this.play);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 571);
+            this.Controls.Add(this.B7);
+            this.Controls.Add(this.B6);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B9);
+            this.Controls.Add(this.B5);
+            this.Controls.Add(this.B8);
+            this.Controls.Add(this.B4);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B4;
+        private System.Windows.Forms.Button B6;
+        private System.Windows.Forms.Button B5;
+        private System.Windows.Forms.Button B7;
+        private System.Windows.Forms.Button B8;
+        private System.Windows.Forms.Button B9;
+    }
+}
+
---
+++ HKBhatiaAssignment1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HKBhatiaAssignment1
+{
+    public partial class Form1 : Form
+    {
+
+        bool chance = true;//checks for x or o turn
+        bool checkwinner = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void play(object sender, EventArgs e)
+        {
+            Button i = (Button)sender;
+
+                if (chance)
+                {
+                    i.Image = Properties.Resources.ImageX;
+                    i.Tag = "1";
+                }
+                else
+                {
+                    i.Image = Properties.Resources.ImageO;
+                    i.Tag = "2";
+                }
+                chance = !chance;
+                i.Enabled = false;
+
+                winner();
+            
+                       
+        }
+
+
+        private void winner()
+        {
+
+            if (B1.Tag == B2.Tag && B2.Tag == B3.Tag && !B1.Enabled)
+                checkwinner = true;
+            else if (B4.Tag == B5.Tag && B5.Tag == B6.Tag && !B4.Enabled)
+                checkwinner = true;
+            else if (B7.Tag == B8.Tag && B8.Tag == B9.Tag && !B7.Enabled)
+                checkwinner = true;
+            else if (B1.Tag == B4.Tag && B4.Tag == B7.Tag && !B1.Enabled)
+                checkwinner = true;
+            else if (B2.Tag == B5.Tag && B5.Tag == B8.Tag && !B2.Enabled)
+                checkwinner = true;
+            else if (B3.Tag == B6.Tag && B6.Tag == B9.Tag && !B3.Enabled)
+                checkwinner = true;
+            else if (B1.Tag == B5.Tag && B5.Tag == B9.Tag && !B1.Enabled)
+                checkwinner = true;
+            else if (B3.Tag == B5.Tag && B5.Tag == B7.Tag && !B3.Enabled)
+                checkwinner = true;
+            
+            if(checkwinner)
+            {
+                String player = "";
+                if (chance)
+                    player = "O";
+                else
+                    player = "X";
+
+                MessageBox.Show(player + " wins the game.");
+            }
+
+            if(checkwinner)
+                gamereset();
+
+        }
+
+        private void gamereset()
+        {
+            checkwinner = false;
+            chance = true;
+            B1.Enabled = true;
+            B2.Enabled = true;
+            B3.Enabled = true;
+            B4.Enabled = true;
+            B5.Enabled = true;
+            B6.Enabled = true;
+            B7.Enabled = true;
+            B8.Enabled = true;
+            B9.Enabled = true;
+            B1.Image = null;
+            B2.Image = null;
+            B3.Image = null;
+            B4.Image = null;
+            B5.Image = null;
+            B6.Image = null;
+            B7.Image = null;
+            B8.Image = null;
+            B9.Image = null;
+            B1.Tag = "1";
+            B2.Tag = "2";
+            B3.Tag = "3";
+            B4.Tag = "4";
+            B5.Tag = "5";
+            B6.Tag = "6";
+            B7.Tag = "7";
+            B8.Tag = "8";
+            B9.Tag = "9";
+
+
+        }
+        
+       
+    }
+
+}
